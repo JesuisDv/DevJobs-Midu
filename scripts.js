7f9c1321be7870dace95861130400aa82c6cf6b1
Jesús Franco <94185768+JesuisDv@users.noreply.github.com>
2025-10-12 06:47:46 +0000

    Agregar configuración de Live Server y script para manejar la aplicación de trabajos

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// const botonApply = document.querySelectorAll(".button-apply-job")
+
+// botonApply.forEach(boton => {
+//     boton.addEventListener("click", ()=>{
+//         boton.textContent= "Aplicado"
+//         boton.style.backgroundColor = "green"
+//         boton.style.cursor = "not-allowed"
+//         boton.disabled = true
+//     })
+// })
+
+const jobsListigSection = document.querySelector(".jobs-listings");
+
+jobsListigSection.addEventListener("click", function(event){
+    console.log(event.target)//Event.target devuelve el elemtno al que se le da click
+    const element = event.target
+
+    if(element.classList.contains("button-apply-job")){
+        element.textContent= "Aplicado"
+        element.style.backgroundColor = "green"
+        element.style.cursor = "not-allowed"
+        element.disabled = true
+    }
+})
